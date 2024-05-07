acebae3a039b8197041c360a8135824751312cda
bboLightyear <158018897+bboLightyear@users.noreply.github.com>
2024-05-07 14:13:26 +0900

    feat print api
    feat print api

--- src/main/java/com/tech/straffic/StrafficRestController.java
+++ src/main/java/com/tech/straffic/StrafficRestController.java
@@ -4,40 +4,66 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tech.straffic.dto.BStorageInfoDto;
 
 @RestController
 public class StrafficRestController {
 	
+	@ResponseBody
 	@RequestMapping(method = RequestMethod.POST,value = "/bstorageinfo")
 	public String bstorageinfo(HttpServletRequest request) throws ClassNotFoundException {
 		System.out.println("bstorageinfo rest con()");
+        
+        String key = "55756f727977687138317466627a7a";    
+        String result = "";
+//        List<BStorageInfoDto> storageList = new ArrayList<>();
 		
-		StringBuilder result = new StringBuilder();
-        try {
-            String apiUrl = "http://openapi.seoul.go.kr:8088/55756f727977687138317466627a7a/json/bikeList/1/10/";
+		try{
 
-            URL url = new URL(apiUrl);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
+			String apiURL = "http://openapi.seoul.go.kr:8088/"+key+"/json/bikeList/1/10/";
+			URL url = new URL(apiURL);
+			BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+			result = bf.readLine();
+			bf.close();
+	        
+	        JSONObject obj = new JSONObject(result);
+	        JSONObject rentBikeStatus = obj.getJSONObject("rentBikeStatus");
+	        JSONObject bresult = rentBikeStatus.getJSONObject("RESULT");
+	        JSONArray rows = rentBikeStatus.getJSONArray("row");
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
-
-            String returnLine;
-
-            while((returnLine = bufferedReader.readLine()) != null) {
-                result.append(returnLine + "\n");
+	        System.out.println("RESULT : " + bresult.getString("CODE"));
+	        System.out.println("RESULT : " + bresult.getString("MESSAGE"));
+	        System.out.println();
+	        
+	        for (int i = 0; i < rows.length(); i++) {
+                JSONObject row = rows.getJSONObject(i);
+                System.out.println("Station Name: " + row.getString("stationName"));
+                System.out.println("Total Racks: " + row.getString("rackTotCnt"));
+                System.out.println("Total Parking Bikes: " + row.getString("parkingBikeTotCnt"));
+                System.out.println("Shared: " + row.getString("shared"));
+                System.out.println("Latitude: " + row.getString("stationLatitude"));
+                System.out.println("Longitude: " + row.getString("stationLongitude"));
+                System.out.println("Station ID: " + row.getString("stationId"));
+                System.out.println();
             }
-            urlConnection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+      	
+        	
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+        
         return result.toString();
 	}
 
